c1abe721b21f476b7c5c8d6862a063a042aaf574
nikita.kirdiapin <nikita.kirdiapin@gmail.com>
2025-06-06 12:14:40 +0200

    added force-dynamic for home page

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,17 +1,19 @@
 import { resolvers, typeDefs } from "@/graphql";
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { NextRequest } from "next/server";
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-const handler = startServerAndCreateNextHandler(server, {
-  context: async () => {
-    // Можно пробрасывать токен, юзера и т.д.
-    return {};
-  },
-});
+const handler = startServerAndCreateNextHandler(server);
 
-export { handler as GET, handler as POST }; // важно!
+export async function GET(request: NextRequest) {
+  return handler(request);
+}
+
+export async function POST(request: NextRequest) {
+  return handler(request);
+}
